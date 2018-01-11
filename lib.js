43bee9f1226dc954752d37e1567d81b626e3a34e
eduardo <eduardo1@net.hr>
2018-01-11 03:56:05 +0100

    refractoring

--- lib.js
+++ lib.js
@@ -1,46 +1,49 @@
 class Http {
     constructor() {}
+
     static async get(url) {
-        return Http._send('GET', url)
+        const s = new Http;
+        return s._send('GET', url)
     }
 
     static post(url, data) {
+        const s = new Http;
         if (!data) {
             throw Error(`No data to send`);
         }
-        return Http._send('POST', url, data);
+        return s._send('POST', url, data);
     }
 
     static put(url, data) {
+        const s = new Http;
         if (!data) {
             throw Error(`No data to send`);
         }
-        return Http._send('PUT', url, data);
+        return s._send('PUT', url, data);
     }
 
     static delete(url) {
-        return Http._send('DELETE', url);
+        const s = new Http;
+        return s._send('DELETE', url);
     }
 
-    static _send(method, url, toSend) {
+    _send(method, url, toSend) {
         return new Promise((resolve, reject) => {
             const xhr = new XMLHttpRequest();
             xhr.open(method, url);
             toSend ? xhr.setRequestHeader("Content-Type", 'application/json') : null
             xhr.onload = function () {
-                if (this.readyState === 4) {
-                    if (this.getResponseHeader('Content-Type').split(" ")[0] === 'application/json;') {
-                        return resolve({
-                            status: this.status,
-                            body: JSON.parse(this.response)
-                        });
-                    } else {
-                        return resolve({
-                            content_type: this.getResponseHeader('Content-Type'),
-                            status: this.status,
-                            body: this.response
-                        });
-                    }
+                if (this.getResponseHeader('Content-Type').split(" ")[0] === 'application/json;') {
+                    return resolve({
+                        status: this.status,
+                        body: JSON.parse(this.response)
+                    });
+                } else {
+                    return resolve({
+                        content_type: this.getResponseHeader('Content-Type'),
+                        status: this.status,
+                        body: this.response
+                    });
                 }
                 if (this.getResponseHeader('Content-Type').split(" ")[0] === 'application/json;') {
                     return reject({
