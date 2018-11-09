76a3a38d54fdb4763627f72590ff57c3e04a7573
Eduardo <eduardo1@net.hr>
2018-11-09 18:47:25 +0100

    small refractor, remove console.logs,remove return statments from reject,reslove

--- lib.js
+++ lib.js
@@ -1,64 +1,68 @@
-const Http = (function () {
+const _Hlib = (function() {
+  function get(url) {
+    return _send("GET", url);
+  }
 
-    function get(url) {
-        return _send('GET', url)
+  function post(url, data) {
+    if (!data) {
+      throw Error(`No data to send`);
     }
+    return _send("POST", url, data);
+  }
 
-    function post(url, data) {
-        if (!data) {
-            throw Error(`No data to send`);
+  function put(url, data) {
+    if (!data) {
+      throw Error(`No data to send`);
+    }
+    return _send("PUT", url, data);
+  }
+
+  function $delete(url) {
+    return _send("DELETE", url);
+  }
+
+  function _send(method, url, toSend) {
+    return new Promise((resolve, reject) => {
+      const xhr = new XMLHttpRequest();
+      xhr.open(method, url);
+      toSend ? xhr.setRequestHeader("Content-Type", "application/json") : null;
+      xhr.onload = function() {
+        if (
+          this.getResponseHeader("Content-Type").split(" ")[0] ===
+          "application/json;"
+        ) {
+          resolve({
+            status: this.status,
+            body: JSON.parse(this.response)
+          });
+        } else {
+          resolve({
+            content_type: this.getResponseHeader("Content-Type"),
+            status: this.status,
+            body: this.response
+          });
         }
-        return _send('POST', url, data);
-    }
-
-    function put(url, data) {
-        if (!data) {
-            throw Error(`No data to send`);
+        if (
+          this.getResponseHeader("Content-Type").split(" ")[0] ===
+          "application/json;"
+        ) {
+          reject({
+            status: this.target.status,
+            body: JSON.parse(data.target.response)
+          });
         }
-        return _send('PUT', url, data);
-    }
-
-    function $delete(url) {
-        return _send('DELETE', url);
-    }
-
-    function _send(method, url, toSend) {
-        return new Promise((resolve, reject) => {
-            const xhr = new XMLHttpRequest();
-            xhr.open(method, url);
-            toSend ? xhr.setRequestHeader("Content-Type", 'application/json') : null
-            xhr.onload = function () {
-                if (this.getResponseHeader('Content-Type').split(" ")[0] === 'application/json;') {
-                    return resolve({
-                        status: this.status,
-                        body: JSON.parse(this.response)
-                    });
-                } else {
-                    return resolve({
-                        content_type: this.getResponseHeader('Content-Type'),
-                        status: this.status,
-                        body: this.response
-                    });
-                }
-                if (this.getResponseHeader('Content-Type').split(" ")[0] === 'application/json;') {
-                    return reject({
-                        status: this.target.status,
-                        body: JSON.parse(data.target.response)
-                    });
-                }
-                console.log(data);
-                reject({
-                    status: this.status,
-                    body: this.target.response
-                })
-            }
-            toSend ? xhr.send(JSON.stringify(toSend)) : xhr.send();
-        })
-    }
-    return {
-        get,
-        post,
-        put,
-        delete: $delete
-    }
-}());
+        reject({
+          status: this.status,
+          body: this.target.response
+        });
+      };
+      toSend ? xhr.send(JSON.stringify(toSend)) : xhr.send();
+    });
+  }
+  return {
+    get,
+    post,
+    put,
+    delete: $delete
+  };
+})();
